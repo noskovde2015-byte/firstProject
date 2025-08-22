b541cf149a14f33c765ca8665fea12486d305f36
Danil <noskovde2015@gmail.com>
2025-08-22 12:33:51 +0300

    Change make admin

--- todo--application/api/apiv1/admin_views.py
+++ todo--application/api/apiv1/admin_views.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
 from core.config import settings
 from core.models import User, Role
 from auth.dependencies import get_current_user
@@ -22,20 +24,24 @@ async def make_admin(
             detail="You are not an admin"
         )
 
+    stmt = select(User).options(selectinload(User.role)).where(User.id == user_id)
+    result = await session.execute(stmt)
+    target_user = result.scalar_one_or_none()
 
-    target_user = await session.get(User, user_id)
     if not target_user:
-        raise HTTPException(status_code=404,
-                            detail="User not found")
+        raise HTTPException(status_code=404, detail="User not found")
 
-    admin_role = await session.scalar(select(Role).where(Role.name == "admin"))
+
+    stmt_role = select(Role).where(Role.name == "admin")
+    result_role = await session.execute(stmt_role)
+    admin_role = result_role.scalar_one_or_none()
+
     if not admin_role:
         raise HTTPException(status_code=500, detail="Admin role not found")
 
-    if target_user.role.name == "admin":
-        raise HTTPException(status_code=403,
-                            detail="User is an admin")
 
+    if target_user.role.name == "admin":
+        raise HTTPException(status_code=403, detail="User is already admin")
 
     target_user.role = admin_role
     await session.commit()
