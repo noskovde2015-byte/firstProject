71e5a4f2d7d2b968a689054ff8a881760846e687
Danil <noskovde2015@gmail.com>
2025-09-02 10:11:01 +0300

    Add CORSmiddlewares

--- todo--application/main.py
+++ todo--application/main.py
@@ -1,14 +1,33 @@
 from fastapi import FastAPI, middleware, Request
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
 from api import router as api_router
 from core.config import settings
 from core.middlewares.middlewares import aut_middleware
 
 
+
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router)
 
 app.middleware("http")(aut_middleware)
+
+
 
 
 
